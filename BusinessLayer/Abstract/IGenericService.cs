be01fd47541d722653ac5c2a6d8d540ead89b33a
Rümeysa Gedik <rumeysa1783@gmail.com>
2022-09-11 00:34:02 +0300

    first commit

---
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IGenericService<T>
+    {
+        //abstract klasöründe tanımladığım bileşenler generic birer service olacaklar. Bu servisler businness tarafında crud operasyonları için gerekli olan metodların çağrılmadan önce imzasını taşıyacak
+        void TAdd(T t);
+        void TDelete(T t);
+        void TUpdate(T t);
+        List<T> TGetList();
+        T TGetById(int id);
+
+    }
+}
---
+++ DataAccessLayer/EntityFramework/EfAboutDal.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Repository;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfAboutDal:GenericRepository<About>, IAboutDal
+    {
+        //ilerleyen zamanlarda temel crud operasyonları dışında sadece bu entity'e ait işlemlerin yapılacağı interface burası olacak.
+    }
+}
